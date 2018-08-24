2a49efbe8b6375e23c538093938f956fc28544d8
Dan Navarro <dannavarro2739@gmail.com>
2018-08-24 11:59:31 -0500

    I am uploading this to my repository. meow

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Bella Rodriguez,Dan Navarro
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Bella Computer Engineering,Dan Computer Science!\n");
+
+  return 0;
+}
